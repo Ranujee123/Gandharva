3237efbcf8c5fef8d41e2c83178d696be9dab146
shyamaljayamanne <shyamalruwantha@gmail.com>
2024-04-17 23:08:53 +0530

    complains model done

---
+++ src/main/java/com/user/model/ComplaintsModel.java
@@ -0,0 +1,115 @@
+package com.user.model;
+
+public class ComplaintsModel {
+    private int idcomplaints;
+    private String userid;
+    private String date;
+    private String title;
+    private String descripion;
+    private String status;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String countryOfResidence;
+    private String userType;
+    private String base64Image;
+
+
+
+
+    public int getIdcomplaints() {
+        return idcomplaints;
+    }
+
+    public void setIdcomplaints(int idcomplaints) {
+        this.idcomplaints = idcomplaints;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescripion() {
+        return descripion;
+    }
+
+    public void setDescripion(String descripion) {
+        this.descripion = descripion;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountryOfResidence() {
+        return countryOfResidence;
+    }
+
+    public void setCountryOfResidence(String countryOfResidence) {
+        this.countryOfResidence = countryOfResidence;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getBase64Image() {
+        return base64Image;
+    }
+
+    public void setBase64Image(String base64Image) {
+        this.base64Image = base64Image;
+    }
+}
